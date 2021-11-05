b7a6a26ec3c5fc168fa3fab301d98a2877f3a708
Marlon Lewis <marlonlewis483@gmail.com>
2021-11-05 12:15:09 -0500

    edited animation in js file

--- world.js
+++ world.js
@@ -7,6 +7,7 @@ window.onload = function(){
         if(e.keyCode==13){
             $.get(myphp()+"&lookup=countries", function(data){
                 $("#result").html(data);
+                $("#result").fadeIn("fast");
                 $("#country").val("");
             });
         }
@@ -16,6 +17,7 @@ window.onload = function(){
         e.preventDefault();
         $.get(myphp()+"&lookup=countries", function(data){
             $("#result").html(data);
+            $("#result").fadeIn("fast");
             $("#country").val("");
         });
     })
@@ -24,6 +26,7 @@ window.onload = function(){
         e.preventDefault();
         $.get(myphp()+"&lookup=cities", function(data){
             $("#result").html(data);
+            $("#result").fadeIn("fast");
             $("#country").val("");
         });
     })
@@ -31,7 +34,7 @@ window.onload = function(){
     $("#clear").click(function(e){
         e.preventDefault();
         $("#result").html("<p>data cleared</p>");
-        $("#result").fadeOut(3000);
+        $("#result").fadeOut(500);
     })
 
 }
